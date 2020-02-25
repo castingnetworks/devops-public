556227cc220ce6f895bba1ee3e9e0c7a86954390
jmalara <jeremy@malara.net>
2020-02-24 18:57:21 -0800

    moving tag

--- terraform/redis/vpc.tf
+++ terraform/redis/vpc.tf
@@ -1,12 +1,10 @@
 data "aws_vpc" "redis" {
-  count   = var.vpc_config == null ? 0 : 1
   tags = {
     "${var.vpc_config.env_tag}" = var.vpc_config.env_value
   }
 }
 
 data "aws_subnet_ids" "redis" {
-  count   = var.vpc_config == null ? 0 : 1
   vpc_id = data.aws_vpc.redis[0].id
   tags = {
     "${var.vpc_config.subnet_tag}" = var.vpc_config.subnet_value
@@ -14,7 +12,6 @@ data "aws_subnet_ids" "redis" {
 }
 
 data "aws_security_groups" "redis" {
-  count   = var.vpc_config == null ? 0 : 1
   tags = {
     "${var.vpc_config.sg_tag}"  = var.vpc_config.sg_value,
     "${var.vpc_config.env_tag}" = var.vpc_config.env_value
