6dd0e3b520f055fe1c5783cd4b8b736b9b35e07c
jmalara <jeremy@malara.net>
2020-03-02 12:37:36 -0800

    updating sg tags

--- terraform/elasticsearch/security_groups.tf
+++ terraform/elasticsearch/security_groups.tf
@@ -26,6 +26,7 @@ resource "aws_security_group_rule" "es_egress" {
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id        = aws_security_group.es_security_group.id
 }
 
