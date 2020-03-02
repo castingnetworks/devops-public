74131a0ae8dc9e2ccf73e99a5bae6471d747f13d
jmalara <jeremy@malara.net>
2020-03-01 23:22:51 -0800

    fixing typo

--- terraform/redis/security_groups.tf
+++ terraform/redis/security_groups.tf
@@ -26,7 +26,7 @@ resource "aws_security_group_rule" "redis_networks_ingress" {
   security_group_id = aws_security_group.redis_security_group.id
 }
 
-resource "aws_security_group_rule" "redis_additional_cidr_ingress" {
+resource "aws_security_group_rule" "redis_cadditional_cidr_ingress" {
   type              = "ingress"
   from_port         = var.redis_port
   to_port           = var.redis_port
