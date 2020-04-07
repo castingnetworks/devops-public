f7d095848ac56c3711b43e0fde5cb5655877d737
Jeremy Malara <jeremy@malara.net>
2020-04-06 17:24:18 -0700

    Update logging.tf

--- terraform/elasticsearch/logging.tf
+++ terraform/elasticsearch/logging.tf
@@ -1,17 +1,17 @@
-resource "aws_cloudwatch_log_group" "es_vpc-search" {
-  name = "/aws/aes/domains/${local.domain_name}/slow-search-logs"
+resource "aws_cloudwatch_log_group" "es_vpc_search" {
+  name = "/aws/aes/domains/${aws_elasticsearch_domain.es[0].domain_name}/slow-search-logs"
 }
 
-resource "aws_cloudwatch_log_group" "es_vpc-index" {
-  name = "/aws/aes/domains/${local.domain_name}/index-logs"
+resource "aws_cloudwatch_log_group" "es_vpc_index" {
+  name = "/aws/aes/domains/${aws_elasticsearch_domain.es[0].es_vpc.domain_name}/index-logs"
 }
 
-resource "aws_cloudwatch_log_group" "es_vpc-error" {
-  name = "/aws/aes/domains/${local.domain_name}/error-logs"
+resource "aws_cloudwatch_log_group" "es_vpc_error" {
+  name = "/aws/aes/domains/${aws_elasticsearch_domain.es_vpc.domain_name}/error-logs"
 }
 
-resource "aws_cloudwatch_log_resource_policy" "example" {
-  policy_name = "tf-${local.domain_name}-policy"
+resource "aws_cloudwatch_log_resource_policy" "es_vpc" {
+  policy_name = "tf-${aws_elasticsearch_domain.es_vpc.domain_name}-policy"
 
   policy_document = <<CONFIG
 {
