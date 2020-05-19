90a9620283fe48776789a60bfc6d63d84a22b875
jmalara <jeremy@malara.net>
2020-05-19 10:02:14 -0700

    adding app_iam

--- terraform/app_iam/main.tf
+++ terraform/app_iam/main.tf
@@ -1,6 +1,6 @@
 # Create IAM Role
 resource "aws_iam_role" "app-role" {
-  name               = "tf-${var.env}-${var.app_name}-role"
+  name               = "tf-${var.name_prefix}-${var.app_name}-role"
   assume_role_policy = data.aws_iam_policy_document.app-assume-role-policy.json
   tags = local.tags
 }
@@ -8,14 +8,14 @@ resource "aws_iam_role" "app-role" {
 
 # Create IAM Policy
 resource "aws_iam_policy" "app-policy" {
-  name   = "tf-${var.env}-${var.app_name}-policy"
+  name   = "tf-${var.name_prefix}-${var.app_name}-policy"
   path   = "/"
   policy = var.policy
 }
 
 
 resource "aws_iam_policy_attachment" "app-policy-attachment" {
-  name       = "tf-${var.env}-${var.app_name}-policy-attach"
+  name       = "tf-${local.name_prefix}-${var.app_name}-policy-attach"
   role       = aws_iam_role.app-role.name
   policy_arn = aws_iam_policy.app-policy.arn
 }
--- terraform/app_iam/variables.tf
+++ terraform/app_iam/variables.tf
@@ -3,8 +3,8 @@ variable "eks_cluster_name" {
   type        = string
 }
 
-variable "env" {
-  description = "Environment Name"
+variable "name_prefix" {
+  description = "Prefix for role name"
   type        = string
 }
 
