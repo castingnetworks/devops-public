66e4abc0aec5f6a9e8989fbee650fb96c8b9a1b4
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2020-05-19 11:30:20 -0700

    Update data.tf

--- terraform/app_iam/data.tf
+++ terraform/app_iam/data.tf
@@ -2,13 +2,17 @@ data "aws_eks_cluster" "eks" {
   name = var.eks_cluster_name
 }
 
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = data.aws_eks_cluster.eks.identity.0.oidc.0.issuer
+}
+
 data "aws_iam_policy_document" "app-assume-role-policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
 
     principals {
-      identifiers = ["${data.aws_eks_cluster.eks.identity.role_arn}"]
+      identifiers = ["${aws_iam_openid_connect_provider.eks.arn}"]
       type        = "Federated"
     }
   }
