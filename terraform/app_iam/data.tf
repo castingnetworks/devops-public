9f711cfad80f9bc2a909d16ec008b2d227c0c6fc
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2020-05-19 11:32:18 -0700

    Update data.tf

--- terraform/app_iam/data.tf
+++ terraform/app_iam/data.tf
@@ -5,6 +5,7 @@ data "aws_eks_cluster" "eks" {
 resource "aws_iam_openid_connect_provider" "eks" {
   url = data.aws_eks_cluster.eks.identity.0.oidc.0.issuer
   thumbprint_list = []
+  client_id_list  = ["sts.amazonaws.com"]
 }
 
 data "aws_iam_policy_document" "app-assume-role-policy" {
