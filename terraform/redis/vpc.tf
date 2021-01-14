197c4ae76c6e0c3789f3b3ebfc612aaa84d896a4
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2021-01-14 12:43:49 -0800

    Update vpc.tf

--- terraform/redis/vpc.tf
+++ terraform/redis/vpc.tf
@@ -4,16 +4,16 @@ data "aws_vpc" "redis" {
   }
 }
 
-data "aws_subnet_ids" "redis" {
-  vpc_id = data.aws_vpc.redis.id
-  tags = {
-    "${var.vpc_config.subnet_tag}" = var.vpc_config.subnet_value
-  }
-}
+#data "aws_subnet_ids" "redis" {
+#  vpc_id = data.aws_vpc.redis.id
+#  tags = {
+ #   "${var.vpc_config.subnet_tag}" = var.vpc_config.subnet_value
+ # }
+#}
 
-data "aws_security_groups" "redis" {
-  tags = {
-    "${var.vpc_config.sg_tag}"  = var.vpc_config.sg_value,
-    "${var.vpc_config.env_tag}" = var.vpc_config.env_value
-  }
-}
+#data "aws_security_groups" "redis" {
+#  tags = {
+#    "${var.vpc_config.sg_tag}"  = var.vpc_config.sg_value,
+ #   "${var.vpc_config.env_tag}" = var.vpc_config.env_value
+ # }
+#}
