dec341071a011897a3ea28bc48b008051d6ddd74
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2021-01-25 12:44:05 -0800

    Update outputs.tf

--- terraform/redis/outputs.tf
+++ terraform/redis/outputs.tf
@@ -6,9 +6,9 @@ output "this_redis_parameter_group" {
   value = aws_elasticache_parameter_group.redis_parameter_group.id
 }
 
-//output "this_redis_subnet_group_name" {
-//  value = aws_elasticache_subnet_group.redis_subnet_group.name
-//}
+output "this_redis_subnet_group_name" {
+  value = aws_elasticache_subnet_group.redis_subnet_group.name
+}
 
 output "this_redis_id" {
   value = aws_elasticache_replication_group.redis.id
