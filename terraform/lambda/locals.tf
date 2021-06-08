f904c4f35571cd417f8e4cdac4d765a3460ad67c
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2021-06-08 13:03:53 -0700

    Update locals.tf

--- terraform/lambda/locals.tf
+++ terraform/lambda/locals.tf
@@ -1,6 +1,6 @@
 locals {
  
- environment_variables = {
+ dynamic environment_variables = {
    for_each = var.environment == null ? [] : [var.environment]
      content {
        variables = environment.value.variables
