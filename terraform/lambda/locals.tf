093ca025cfed1f1521cc216d45f035969d641c57
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2021-06-08 13:14:23 -0700

    Delete locals.tf

--- terraform/lambda/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
- 
-  environment_variables = {
-   for_each = var.environment == null ? [] : [var.environment]
-     content = {
-       variables = environment.value.variables
-     }
- }
-}
