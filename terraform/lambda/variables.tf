05550509931d541a6f33434ed976798870def85a
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2021-06-08 11:09:18 -0700

    Update variables.tf

--- terraform/lambda/variables.tf
+++ terraform/lambda/variables.tf
@@ -3,9 +3,9 @@ variable "function_name" {
   type = string
 }
 
-variable "dt_layer_arn" {
-  type = string
-  default = "arn:aws:lambda:us-west-2:725887861453:layer:Dynatrace_OneAgent_1_215_1_20210326-040705_nodejs:1"
+variable "lambda_layers" {
+  type = list
+  default = ["arn:aws:lambda:us-west-2:725887861453:layer:Dynatrace_OneAgent_1_215_1_20210326-040705_nodejs:1"]
 }
 
 variable "handler" {
