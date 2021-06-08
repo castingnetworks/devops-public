74b79f50109f6561f46c195bc898fbebd8e657ac
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2021-06-08 11:01:52 -0700

    Update main.tf

--- terraform/lambda-dt/main.tf
+++ terraform/lambda-dt/main.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "lambda" {
   s3_key                         = var.artifact_zip_key
   
   # Dynatrace extension
-  layers = [var.dt_layer_arn]
+  layers = var.lambda_layers
   
   # Add dynamic blocks based on variables.
 
