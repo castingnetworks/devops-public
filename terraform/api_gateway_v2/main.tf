bbbc20df9ccc86f60fbed84d2dc03063d3192c42
Tomasz Jezierski <tomasz.jezierski@castingnetworks.com>
2021-09-02 12:43:22 +0200

    AWS API Gateway V2

---
+++ terraform/api_gateway_v2/main.tf
@@ -0,0 +1,49 @@
+resource "aws_apigatewayv2_api" "api" {
+  name          = var.name
+  protocol_type = "HTTP"
+  description   = var.name
+  target        = var.lambda_arn
+  tags          = var.tags
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "api" {
+  api_id        = aws_apigatewayv2_api.api.id
+  name          = var.name
+  tags          = var.tags
+}
+
+resource "aws_apigatewayv2_domain_name" "api" {
+  domain_name       = var.hostname
+  domain_name_configuration {
+    certificate_arn = var.acm_cert_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+  tags              = var.tags
+}
+
+resource "aws_apigatewayv2_api_mapping" "api" {
+  api_id        = aws_apigatewayv2_api.api.id
+  domain_name   = aws_apigatewayv2_domain_name.api.id
+  stage         = aws_apigatewayv2_stage.api.id
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = var.route53_zone_id
+  name    = aws_apigatewayv2_domain_name.api.domain_name
+  type    = "A"
+
+  alias {
+    name                   = element(tolist(aws_apigatewayv2_domain_name.api.domain_name_configuration), 0).target_domain_name
+    zone_id                = element(tolist(aws_apigatewayv2_domain_name.api.domain_name_configuration), 0).hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
