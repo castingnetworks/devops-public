70f92522b56622f5a1844120668cf316051174b7
Tomasz Jezierski <tomasz.jezierski@castingnetworks.com>
2021-09-02 15:30:23 +0200

    AWS API Gateway V2 pt2

--- terraform/api_gateway_v2/main.tf
+++ terraform/api_gateway_v2/main.tf
@@ -13,12 +13,6 @@ resource "aws_lambda_permission" "apigw_lambda" {
   source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
 
-resource "aws_apigatewayv2_stage" "api" {
-  api_id        = aws_apigatewayv2_api.api.id
-  name          = var.name
-  tags          = var.tags
-}
-
 resource "aws_apigatewayv2_domain_name" "api" {
   domain_name       = var.hostname
   domain_name_configuration {
@@ -32,7 +26,7 @@ resource "aws_apigatewayv2_domain_name" "api" {
 resource "aws_apigatewayv2_api_mapping" "api" {
   api_id        = aws_apigatewayv2_api.api.id
   domain_name   = aws_apigatewayv2_domain_name.api.id
-  stage         = aws_apigatewayv2_stage.api.id
+  stage         = "$default"
 }
 
 resource "aws_route53_record" "api" {
