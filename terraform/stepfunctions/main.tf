9823154b944388646884a6ba93d891c0482dbb53
Levan Gabeskiria <levangabeskiria@gmail.com>
2021-11-10 15:56:54 +0300

    fix(stepfunctions): revert tracing configuration

--- terraform/stepfunctions/main.tf
+++ terraform/stepfunctions/main.tf
@@ -10,8 +10,4 @@ resource "aws_sfn_state_machine" "state_machine" {
     log_destination        = var.type == "EXPRESS" ? var.log_destination : null
     include_execution_data = var.type == "EXPRESS" ? var.include_execution_data : null
   }
-
-  tracing_configuration {
-    enabled = var.tracing_enabled
-  }
 }
