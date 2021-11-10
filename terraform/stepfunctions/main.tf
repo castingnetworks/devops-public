f73e90190c249f2c0634db45e4f64036aa659255
Levan Gabeskiria <levangabeskiria@gmail.com>
2021-11-10 17:09:18 +0300

    feat: express stepfunction logs

--- terraform/stepfunctions/main.tf
+++ terraform/stepfunctions/main.tf
@@ -5,9 +5,18 @@ resource "aws_sfn_state_machine" "state_machine" {
   tags       = var.tags
   type       = var.type
 
-  logging_configuration {
-    level                  = var.type == "EXPRESS" ? var.log_level : null
-    log_destination        = var.type == "EXPRESS" ? var.log_destination : null
-    include_execution_data = var.type == "EXPRESS" ? var.include_execution_data : null
+  dynamic "logging_configuration" {
+    for_each = var.type == "EXPRESS" && var.log_level != null ? [true] : []
+    content {
+      level                  = var.log_level
+      log_destination        = "${aws_cloudwatch_log_group.stfun[0].arn}:*"
+      include_execution_data = var.include_execution_data
+    }
   }
+}
+
+resource "aws_cloudwatch_log_group" "stfun" {
+  name = "${var.name}-logs"
+  tags = var.tags
+  count = var.type == "EXPRESS" && var.log_level != null ? 1 : 0
 }
