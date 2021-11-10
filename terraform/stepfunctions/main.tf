e4503a5f977af431f51cdb39e15d3f91175a6f55
Levan Gabeskiria <levangabeskiria@gmail.com>
2021-11-10 15:12:35 +0300

    fix(stepfunctions): fix vars

--- terraform/stepfunctions/main.tf
+++ terraform/stepfunctions/main.tf
@@ -6,12 +6,12 @@ resource "aws_sfn_state_machine" "state_machine" {
   type       = var.type
 
   logging_configuration {
-    level                  = var.logging != null ? var.logging.level : null
-    log_destination        = var.logging != null ? var.logging.log_destination : null
-    include_execution_data = var.logging != null ? var.logging.include_execution_data : null
+    level                  = var.type == "EXPRESS" ? var.log_level : null
+    log_destination        = var.type == "EXPRESS" ? var.log_destination : null
+    include_execution_data = var.type == "EXPRESS" ? var.include_execution_data : null
   }
 
   tracing_configuration {
-    enabled = var.tracing != null ? var.tracing.enabled : null
+    enabled = var.tracing_enabled
   }
 }
--- terraform/stepfunctions/variables.tf
+++ terraform/stepfunctions/variables.tf
@@ -13,17 +13,17 @@ variable "definition" {
 }
 
 variable "policy_json" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
   description = "(Deprecated) Use 'policy' variable instead"
 }
 
 variable "policy" {
   description = "An additional policy to attach to the State machine role"
-  type = object({
+  type        = object({
     json = string
   })
-  default = null
+  default     = null
 }
 
 variable "type" {
@@ -32,18 +32,26 @@ variable "type" {
   description = "(Optional) Determines whether a Standard or Express state machine is created. The default is 'STANDARD'. You cannot update the type of a state machine once it has been created. Valid values: 'STANDARD', 'EXPRESS'"
 }
 
-variable "logging" {
-  type = object({
-    level = optional(string)
-    log_destination = optional(string)
-    include_execution_data = optional(bool)
-  })
-  default = null
+variable "log_level" {
+  type        = string
+  default     = "OFF"
+  description = "(Optional) Defines which category of execution history events are logged. Valid values: ALL, ERROR, FATAL, OFF"
 }
 
-variable "tracing" {
-  type = object({
-    enabled = optional(bool)
-  })
-  default = null
+variable "log_destination" {
+  type        = string
+  default     = null
+  description = "(Optional) Amazon Resource Name (ARN) of a CloudWatch log group. Make sure the State Machine has the correct IAM policies for logging. The ARN must end with :*"
+}
+
+variable "include_execution_data" {
+  type        = bool
+  default     = false
+  description = "(Optional) Determines whether execution data is included in your log. When set to false, data is excluded."
+}
+
+variable "tracing_enabled" {
+  type        = bool
+  default     = false
+  description = "(Optional) When set to true, AWS X-Ray tracing is enabled. Make sure the State Machine has the correct IAM policies for logging."
 }
