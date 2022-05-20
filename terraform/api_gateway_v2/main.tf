a6ce2d976d7da95fcc23de3e8359881b4a693bd9
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2022-05-20 13:08:06 -0700

    Update main.tf

--- terraform/api_gateway_v2/main.tf
+++ terraform/api_gateway_v2/main.tf
@@ -44,7 +44,7 @@ resource "aws_route53_record" "api" {
 resource "aws_apigatewayv2_integration" "api" {
   api_id           = aws_apigatewayv2_api.api.id
   description      = "Integration"
-  integration_type = "AWS"
+  integration_type = "AWS_PROXY"
   integration_uri  = var.lambda_arn
 
   integration_method = "POST"
