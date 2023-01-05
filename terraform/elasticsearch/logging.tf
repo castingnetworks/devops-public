460c0714eb3cfd7000889838beadda69907ddae8
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2023-01-05 14:29:39 -0800

    Update logging.tf

--- terraform/elasticsearch/logging.tf
+++ terraform/elasticsearch/logging.tf
@@ -10,7 +10,6 @@ resource "aws_cloudwatch_log_group" "es_vpc_error" {
   name = "/aws/aes/domains/${local.domain_name}/error-logs"
 }
 
-/*
 resource "aws_cloudwatch_log_resource_policy" "es_vpc" {
   policy_name = "tf-${local.domain_name}-policy"#
 
@@ -34,4 +33,3 @@ resource "aws_cloudwatch_log_resource_policy" "es_vpc" {
  }
  CONFIG
  }
-*/
