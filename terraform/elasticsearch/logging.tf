0180cc1c0a537b2ecdb3faf89365ccf92149cdbc
Jeremy Malara <7083753+jmalara@users.noreply.github.com>
2023-01-05 14:36:34 -0800

    Update logging.tf

--- terraform/elasticsearch/logging.tf
+++ terraform/elasticsearch/logging.tf
@@ -10,28 +10,22 @@ resource "aws_cloudwatch_log_group" "es_vpc_error" {
   name = "/aws/aes/domains/${local.domain_name}/error-logs"
 }
 
-/*
-resource "aws_cloudwatch_log_resource_policy" "es_vpc" {
-  policy_name = "tf-${local.domain_name}-policy"#
+data "aws_iam_policy_document" "elasticsearch-log-publishing-policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutLogEventsBatch",
+    ]
+    resources = ["arn:aws:logs:*"]
+    principals {
+      identifiers = ["es.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+resource "aws_cloudwatch_log_resource_policy" "elasticsearch-log-publishing-policy" {
+  policy_document = "${data.aws_iam_policy_document.elasticsearch-log-publishing-policy.json}"
+  policy_name     = "tf-${local.domain_name}-policy"
+}
 
-  policy_document = <<CONFIG
-{
-  "Version": "2012-10-17",
-  "Statement": [
-   {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "es.amazonaws.com"
-      },
-      "Action": [
-         "logs:PutLogEvents",
-         "logs:PutLogEventsBatch",
-         "logs:CreateLogStream"
-       ],
-       "Resource": "arn:aws:logs:*"
-     }
-   ]
- }
- CONFIG
- }
-*/
