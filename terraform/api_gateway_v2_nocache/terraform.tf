6908db5fc899d5c54c57a217d64630f739fe5dfe
Dilip Jadav <dilip.jadav@castingnetworks.com>
2023-12-21 15:03:58 +0530

    Allowed cross account Route53 access

--- terraform/api_gateway_v2_nocache/terraform.tf
+++ terraform/api_gateway_v2_nocache/terraform.tf
@@ -1,3 +1,14 @@
 terraform {
   required_version = ">= 0.12.0"
 }
+
+# Cross account AWS Access
+provider "aws" {
+  alias  = "root"
+  region = var.region
+  assume_role {
+    role_arn     = "arn:aws:iam::463546384433:role/terraform-xaccount"
+    session_name = "terraform-xaccount"
+    external_id  = "terraform"
+  }
+}
