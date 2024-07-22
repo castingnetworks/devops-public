3ae3560b5f1921fa488e4c9406440f43fb705b55
Tomasz Jezierski <tomasz.jezierski@castingnetworks.com>
2024-07-22 20:11:59 +0200

    Revert "assume_role_with_web_identity"
    This reverts commit 0cafd329cf143db9fa2d5c89ccb532c51e613b21.

--- terraform/api_gateway_v2_nocache/terraform.tf
+++ terraform/api_gateway_v2_nocache/terraform.tf
@@ -6,8 +6,9 @@ terraform {
 provider "aws" {
   alias  = "root"
   region = var.region
-  assume_role_with_web_identity {
+  assume_role {
     role_arn     = var.role_arn
     session_name = "terraform-xaccount"
+    external_id  = "terraform"
   }
 }
