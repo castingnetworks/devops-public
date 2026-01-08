44c2264804e902caf4633982f921ceaf3356e98e
djadav-devops <dilip.jadav@castingnetworks.com>
2026-01-08 21:33:12 +0530

    Update subnet resource

--- terraform/redis/vpc.tf
+++ terraform/redis/vpc.tf
@@ -5,7 +5,11 @@ data "aws_vpc" "redis" {
 }
 
 data "aws_subnets" "redis" {
-  vpc_id = data.aws_vpc.redis.id
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.redis.id]
+  }
+
   tags = {
     (var.vpc_config.subnet_tag) = var.vpc_config.subnet_value
   }
@@ -13,7 +17,8 @@ data "aws_subnets" "redis" {
 
 data "aws_security_groups" "redis" {
   tags = {
-    (var.vpc_config.sg_tag)  = var.vpc_config.sg_value,
+    (var.vpc_config.sg_tag)  = var.vpc_config.sg_value
     (var.vpc_config.env_tag) = var.vpc_config.env_value
   }
 }
+
