7bfe3609171dfbe0807d6fa1558932314c51de4e
Dilip Jadav <54315651+djadav-devops@users.noreply.github.com>
2026-01-08 21:21:19 +0530

    Updated subnets

--- terraform/redis/vpc.tf
+++ terraform/redis/vpc.tf
@@ -4,7 +4,7 @@ data "aws_vpc" "redis" {
   }
 }
 
-data "aws_subnet_ids" "redis" {
+data "aws_subnets" "redis" {
   vpc_id = data.aws_vpc.redis.id
   tags = {
     (var.vpc_config.subnet_tag) = var.vpc_config.subnet_value
