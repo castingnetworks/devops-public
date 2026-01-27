f959cfd5607acf2b6c80fc20495304ad94b86b07
Dilip Jadav <54315651+djadav-devops@users.noreply.github.com>
2026-01-27 10:49:34 +0530

    Updated min TLS policy

--- terraform/elasticsearch/vpc.tf
+++ terraform/elasticsearch/vpc.tf
@@ -63,7 +63,7 @@ resource "aws_elasticsearch_domain" "es_vpc" {
   
   domain_endpoint_options {
     enforce_https = true 
-    tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
   cluster_config {
